937782d98ca359e186b483e3ef8649bf36e23543
Vishal Koshti <koshtivishal89@gmail.com>
2018-11-30 21:54:33 +0530

    Update MainActivity.java

--- app/src/main/java/com/easylife/lrc/lyricsview/MainActivity.java
+++ app/src/main/java/com/easylife/lrc/lyricsview/MainActivity.java
@@ -35,4 +35,71 @@ public class MainActivity extends AbstractBaseActivity {
     protected void onViewReady(Bundle savedInstance, Intent intent) {
         getActivityComponent().inject(this);
     }
+        private class CategoryPageAdapter extends PagerAdapter {
+        private final FragmentManager fragmentManager;
+
+        private FragmentTransaction currentTransaction = null;
+
+        private CategoryPageAdapter(FragmentManager fragmentManager) {
+            this.fragmentManager = fragmentManager;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            if (currentTransaction == null) {
+                currentTransaction = fragmentManager.beginTransaction();
+            }
+
+            TabLayout.Tab tab = tabLayout.getTabAt(position);
+
+            Fragment fragment = null;
+            if (tab != null) {
+                fragment = fragmentManager.findFragmentByTag((String)tab.getTag());
+            }
+            // Check if the fragment is already initialized
+            if (fragment != null) {
+                // If it exists, simply attach it in order to show it
+                currentTransaction.attach(fragment);
+            } else {
+                // If not, instantiate and add it to the activity
+                fragment = instantiateFragment(tab);
+                currentTransaction.add(container.getId(), fragment, (String)tab.getTag());
+            }
+
+            return fragment;
+        }
+
+        Fragment instantiateFragment(TabLayout.Tab tab) {
+            return Fragment.instantiate(MusicBrowserActivity.this, (String)tab.getTag());
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            if (currentTransaction == null) {
+                currentTransaction = fragmentManager.beginTransaction();
+            }
+            // Detach the fragment, because another one is being attached
+            currentTransaction.detach((Fragment)object);
+        }
+
+        @Override
+        public void finishUpdate(@NonNull ViewGroup container) {
+            if (currentTransaction != null) {
+                currentTransaction.commit();
+                currentTransaction = null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return tabLayout.getTabCount();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return ((Fragment)object).getView() == view;
+        }
+    }
+
 }
